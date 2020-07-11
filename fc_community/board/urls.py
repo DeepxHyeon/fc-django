1c86078d2f55706359633c8d69ad1b6c64a120ea
DeepxHyeon <deepxhyeon@gmail.com>
2020-07-11 22:47:14 +0900

    05. 게시판 - 02. 게시판 만들기 - 2

--- fc_community/board/urls.py
+++ fc_community/board/urls.py
@@ -16,5 +16,7 @@ Including another URLconf
 from django.urls import path, include
 from . import views
 urlpatterns = [
-    path('list/', views.board_list)   
+    path('detail/<int:pk>/', views.board_detail),
+    path('list/', views.board_list),
+    path('write/', views.board_write)   
 ]
