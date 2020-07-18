81eb7082dbb9d8f30ff567a3834cc25fe0502d1e
DeepxHyeon <deepxhyeon@gmail.com>
2020-07-18 19:40:51 +0900

    06. 태그 - 02. 태그 만들기 - 2

--- fc_community/board/views.py
+++ fc_community/board/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.core.paginator import Paginator
 from django.http import Http404
 from fcuser.models import Fcuser
+from tag.models import Tag
 from .models import Board
 from .forms import BoardForm
 
@@ -18,18 +19,28 @@ def board_detail(request, pk):
 def board_write(request):
     if not request.session.get('user'):
         return redirect('/fcuser/login')
+
     if request.method == 'POST':
         form = BoardForm(request.POST)
         if form.is_valid():
             user_id = request.session.get('user')
             fcuser = Fcuser.objects.get(pk=user_id)
 
+            tags = form.cleaned_data['tags'].split(',')
+
             board = Board()
             board.title = form.cleaned_data['title']
             board.contents = form.cleaned_data['contents']
             board.writer = fcuser
             board.save()
-            
+
+            for tag in tags:
+                if not tag:
+                    continue
+
+                _tag, _ = Tag.objects.get_or_create(name=tag)
+                board.tags.add(_tag)
+
             return redirect('/board/list/')
     
     else:
